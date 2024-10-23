c4100f0fd6d8df05021fe441e8fb99263b36dc3b
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-10-23 09:59:53 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -171,9 +171,6 @@ TW_INCLUDE_CRYPTO := false
 TW_INCLUDE_CRYPTO_FBE := false
 TW_INCLUDE_FBE_METADATA_DECRYPT := false
 
-# Fix fastboot reboot
-TW_NO_FASTBOOT_BOOT := true
-
 # MTP
 TW_MTP_DEVICE := /dev/mtp_usb
 TW_HAS_MTP := true
